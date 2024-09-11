0529b2ef8b37a9badf9ddcdb54ed512785422222
Daniel Teo <dteo318@gmail.com>
2024-09-10 19:38:44 -0700

    Implement my banks page to view bank accounts and copy their shareable IDs

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,6 +2,7 @@ import { formatAmount } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import Copy from "./Copy";
 
 const BankCard = ({
   userName,
@@ -10,7 +11,10 @@ const BankCard = ({
 }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
@@ -49,6 +53,8 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title={account?.shareableId} />}
     </div>
   );
 };
